92bf784715a817ce14995d05d67a3cae51264c4c
rami <rami4luv1987@gmail.com>
2017-08-07 11:19:54 +0200

    finish

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ fetch('./bands.json')
 
 function whenJSONLoaded(bands) {
   var bandslist = bands.map(function(band){
+    var memberslist = renderMembers(band.members);
+    var albumslist = renderAlbums(band.albums);
     return `<section>
       <header>
         <h1>${band.name}</h1>
@@ -16,31 +18,11 @@ function whenJSONLoaded(bands) {
       </header>
 
       <div class="members">
-        <h4>Members</h4>
-        <ul>
-          <li>
-            <span class="member-name">John Doe</span>
-            <span class="member-instrument">Guitar</span>
-          </li>
-          <li>
-            <span class="member-name">John Doe</span>
-            <span class="member-instrument">Guitar</span>
-          </li>
-        </ul>
+          ${memberslist}
       </div>
 
       <div class="albums">
-        <h4>Albums</h4>
-        <ul>
-          <li>
-            <span class="album-name">Album Name</span>
-            <span class="album-release-year">(2001)</span>
-          </li>
-          <li>
-            <span class="album-name">Album Name</span>
-            <span class="album-release-year">(2001)</span>
-          </li>
-        </ul>
+        ${albumslist}
       </div>
     </section>`
   }).join('');
@@ -51,7 +33,44 @@ function whenJSONLoaded(bands) {
 var mainElement = document.querySelector("#main");
 
 
+function renderMembers(members) {
+  var  htmlString = "<h4>Members</h4><ul>";
+  members.sort(sortByName).forEach(function(member){
+    htmlString += `
+        
+          <li>
+            <span class="member-name">${member.name}</span>
+            <span class="member-instrument">${member.instrument}</span>
+          </li>`
+  });
+  return htmlString + "</ul>";
+}
 
+function renderAlbums(albums) {
+  var htmlString = "<h4>Albums</h4><ul>";
+  albums.sort(sortByNumber).forEach(function(album) {
+    
+    htmlString += `
+          
+          <li>
+            <span class="album-name">${album.title}</span>
+            <span class="album-release-year">${album.releaseYear}</span>
+          </li>`
+  });
+  return htmlString + "</ul>";
+}
+
+function sortByName(a,b) {
+  if (a.name < b.name)
+     return -1;
+  if (a.name > b.name)
+    return 1;
+  return 0;
+}
+
+function sortByNumber(a,b) {
+  return a.releaseYear - b.releaseYear;
+}
 
 
 
