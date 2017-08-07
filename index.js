fb45ed5b039e14735710b6fa72e6212950586944
rami <rami4luv1987@gmail.com>
2017-08-07 09:50:53 +0200

    styp3

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-var url = "bands.json";
+fetch('./bands.json')
+  .then(function(response) {
+  return response.json();
+})
+.then(whenJSONLoaded)
+.catch(function(err) {
+  throw err;
+});
 
-fetch(url)
-  .then(function(response){
-    console.log(response);
-    return response.json();
-  })
-  .then(function(json){
-    console.info(json[2]);
-  })
-  .catch(function(err){
-    console.info(err);
-  });
+function whenJSONLoaded(bands) {
+  var bandslist = bands.map(function(band){
+    return `<section>
+      <header>
+        <h1>${band.name}</h1>
+        <h2>${band.genre}</h2>
+      </header>
+
+      <div class="members">
+        <h4>Members</h4>
+        <ul>
+          <li>
+            <span class="member-name">John Doe</span>
+            <span class="member-instrument">Guitar</span>
+          </li>
+          <li>
+            <span class="member-name">John Doe</span>
+            <span class="member-instrument">Guitar</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="albums">
+        <h4>Albums</h4>
+        <ul>
+          <li>
+            <span class="album-name">Album Name</span>
+            <span class="album-release-year">(2001)</span>
+          </li>
+          <li>
+            <span class="album-name">Album Name</span>
+            <span class="album-release-year">(2001)</span>
+          </li>
+        </ul>
+      </div>
+    </section>`
+  }).join('');
+  
+  mainElement.innerHTML = bandslist;
+}
+
+var mainElement = document.querySelector("#main");
+
+
+
+
+
+
+
+
+
+
